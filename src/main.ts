06d26beb7a2fbd6b038962ada6196bdff6fbba48
sunhuihuia <97937992+sunhuihuia@users.noreply.github.com>
2024-09-12 10:50:39 +0800

    微信浏览器文案修改

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ var ua = navigator.userAgent.toLowerCase();
   var isWeixin = ua.indexOf('micromessenger') != -1;
   if (isWeixin) {
     document.head.innerHTML = '<title>抱歉，出错了</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0"><link rel="stylesheet" type="text/css" href="https://res.wx.qq.com/open/libs/weui/0.4.1/weui.css">';
-    document.body.innerHTML = '<div class="weui_msg"><div class="weui_icon_area"><i class="weui_icon_info weui_icon_msg"></i></div><div class="weui_text_area"><h4 class="weui_msg_title">请用浏览器打开</h4></div></div>';
+    document.body.innerHTML = '<div class="weui_msg"><div class="weui_icon_area"><i class="weui_icon_info weui_icon_msg"></i></div><div class="weui_text_area"><h4 class="weui_msg_title">请不要用微信浏览器打开！请将网址复制到其他浏览器打开</h4></div></div>';
   }
 app.config.globalProperties.$sqlWork = async function async(
   CommandType: any,
